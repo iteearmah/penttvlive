02466ee9b2ae7e226c9cd816db662d7de04564fd
Samuel <samuel.armah@webad.com.gh>
2016-04-21 12:41:45 +0000

    add media plugin

--- src/mainapp.js
+++ src/mainapp.js
@@ -1,20 +1,34 @@
+var IMAGE_PATH='src/images/';
+var PAGE_MARGIN = 16;
 var page = new tabris.Page({
-  title: "Pent TV Live",
+  title: "PentTV Live",
   topLevel: true
 });
+var drawer = new tabris.Drawer();
 tabris.ui.set("background", "#0032D5");
-var button = new tabris.Button({
-  text: "Native Widgets",
-  layoutData: {centerX: 0, top: 100}
-}).appendTo(page);
 
-var label = new tabris.TextView({
-  font: "24px",
-  layoutData: {centerX: 0, top: [button, 50]}
+var tabFolder = tabris.create("TabFolder", {
+  left: 0, top: 0, right: 0, bottom: 0,
+  background: "#0032D5",
+  textColor: "white",
+  elevation: 4
 }).appendTo(page);
+ 
+var createTab = function(title, image) {
+  var tabObj=tabris.create("Tab", {
+    title: title,
+    image: {src: image},
+    background: "#ccc"
+  }).appendTo(tabFolder);
+  return tabObj;
+};
 
-button.on("select", function() {
-  label.set("text", "Pent TV Live Rock!");
-});
+var tvTab=createTab('TV',IMAGE_PATH+'widescreen-tv-48.png');
+var tvnewsTab=createTab('News',IMAGE_PATH+'news-48.png');
+
+new tabris.Video({
+  layoutData: {left: 0, right: 0, top: 0, bottom: 0},
+  url: "http://www.sample-videos.com/video/mp4/240/big_buck_bunny_240p_1mb.mp4"
+}).appendTo(tvTab);
 
 page.open();
